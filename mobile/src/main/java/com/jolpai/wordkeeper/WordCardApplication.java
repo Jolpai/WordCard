d4f6277b78bf8682f9f6d172702362f333c8b77f
Tanim reja <tanim2reja@gmail.com>
2018-08-06 16:30:51 +0600

    ...

--- mobile/src/main/java/com/jolpai/wordkeeper/WordCardApplication.java
+++ mobile/src/main/java/com/jolpai/wordkeeper/WordCardApplication.java
@@ -1,22 +1,27 @@
 package com.jolpai.wordkeeper;
 
-import android.app.Application;
-
 import com.facebook.stetho.Stetho;
 
+import dagger.android.AndroidInjector;
+import dagger.android.DaggerApplication;
 import io.realm.Realm;
 
 /**
  * Created by Tanim reja on 10/10/2016.
  */
 
-public class WordCardApp extends Application {
+public class WordCardApplication extends DaggerApplication {
 
     public void onCreate() {
         super.onCreate();
         Stetho.initializeWithDefaults(this);
 
         Realm.init(this);
+    }
+
+    @Override
+    protected AndroidInjector<? extends DaggerApplication> applicationInjector() {
+        return null;
     }
 
 
